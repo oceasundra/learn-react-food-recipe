6bdd14b2a555950038e7d46dd902da6f78060770
Firda Rahayu <firdarahayu105@gmail.com>
2023-07-21 16:18:59 +0700

    Flavor Verse

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import RecipeDetail from "./pages/RecipeDetail";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-black">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="recipes/:id" element={<RecipeDetail />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
